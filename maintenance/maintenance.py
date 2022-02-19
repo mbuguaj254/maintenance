dd5fbd866bf92d37cd79e7e52c3f2b701314b6ef
mbuguaj254 <mbuguaj58@gmail.com>
2022-02-19 10:41:07 +0300

    maintenance- my first commit

--- maintenance/maintenance.py
+++ maintenance/maintenance.py
@@ -1,4 +1,6 @@
+#importing pysimplegui for the unterface
 import PySimpleGUI as sg
+#importing pandas for interacting with excel
 import pandas as pd
 
 # Add some color to the window
@@ -8,19 +10,19 @@ EXCEL_FILE = 'Data_Entry.xlsx'
 df = pd.read_excel(EXCEL_FILE)
 
 layout = [
-    [sg.Text('Please fill out the following fields:')],
-    [sg.Text('No.', size=(15,1)), sg.InputText(key='No.')],
-    [sg.Text('User', size=(15,1)), sg.InputText(key='User')],
-    [sg.Text('Division/Department', size=(15,1)), sg.InputText(key='Division/Department')],
-    [sg.Text('Equipment description', size=(15,1)), sg.Combo(['Laptop', 'Desktop', 'IP Phone', 'Monitor', 'Printer'], key='Equipment description')],
-    [sg.Text('Eqpt. make & model', size=(15,1)), sg.InputText(key='Equipment make & model')],
-    [sg.Text('Serial No.', size=(15,1)), sg.InputText(key='Serial No.')],
-    [sg.Text('GDC Tag No.', size=(15,1)), sg.InputText(key='GDC Tag No.')],    
-    [sg.Text('Hardware Specs.', size=(15,1)), sg.InputText(key='Hardware Specification')],
-    [sg.Text('Software/Apps', size=(15,1)), sg.InputText(key='Software/Applications')],
-    [sg.Text('Status', size=(15,1)), sg.InputText(key='Status')],
-    [sg.Text('Remarks/Comments', size=(15,1)), sg.InputText(key='Technician Remarks/Comments')],                        
-    [sg.Submit(), sg.Button('Clear'), sg.Exit()]
+    [sg.Text('Please fill out the following fields:')], #plain text
+    [sg.Text('No.', size=(15,1)), sg.InputText(key='No.')], #input text values
+    [sg.Text('User', size=(15,1)), sg.InputText(key='User')], #input text values
+    [sg.Text('Division/Department', size=(15,1)), sg.InputText(key='Division/Department')], #input text values
+    [sg.Text('Equipment description', size=(15,1)), sg.Combo(['Laptop', 'Desktop', 'IP Phone', 'Monitor', 'Printer'], key='Equipment description')], #drop down options
+    [sg.Text('Eqpt. make & model', size=(15,1)), sg.InputText(key='Equipment make & model')],#input text values
+    [sg.Text('Serial No.', size=(15,1)), sg.InputText(key='Serial No.')],#input text values
+    [sg.Text('GDC Tag No.', size=(15,1)), sg.InputText(key='GDC Tag No.')],    #input text values
+    [sg.Text('Hardware Specs.', size=(15,1)), sg.InputText(key='Hardware Specification')],#input text values
+    [sg.Text('Software/Apps', size=(15,1)), sg.InputText(key='Software/Applications')],#input text values
+    [sg.Text('Status', size=(15,1)), sg.InputText(key='Status')],#input text values
+    [sg.Text('Remarks/Comments', size=(15,1)), sg.InputText(key='Technician Remarks/Comments')],    #input text values                    
+    [sg.Submit(), sg.Button('Clear'), sg.Exit()] #buttons
 ]
 
 window = sg.Window('Maintenance sheet', layout)
